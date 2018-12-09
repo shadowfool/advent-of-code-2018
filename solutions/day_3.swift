631c241553d601e92941de613b37ff1587ff0982
Aaron Freidus <aaron.freidus@energybox.com>
2018-12-09 13:26:37 -0500

    clojure is hard

--- solutions/day_3.swift
+++
@@ -1,3 +0,0 @@
-// Someone told me I could run swift from the cli, 
-// and I've only got 4 hours to complete this one so why not.
-
---
+++ solutions/day_4.clj
@@ -0,0 +1,24 @@
+( require '[clojure.string :as str] )
+( def input ( slurp "inputs/day_4.txt" ) )
+
+
+( def sorted_list 
+    ( sort-by 
+        :date
+        ( map 
+            ( 
+                fn s ( 
+                    { :date ( get ( str/split s #"" ) 0 ) } 
+                )
+            )
+            ( clojure.string/split-lines input )
+        ) 
+    ) 
+)
+( print sorted_list )
+
+; clojure.string/split "" #"]"
+; {
+;     :date ( get arr 0 )
+;     :message ( get arr 1 )
+; }
